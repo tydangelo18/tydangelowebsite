ecf8f8ff24dacd670b851ff19db578c63a85c855
Ty D'Angelo <tydangelo18@gmail.com>
2024-12-13 11:21:45 -0600

    Fix UI work cards.

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,11 +1,29 @@
-import ShareplateMockup from "./shareplate.jpeg"
-import KwilUi from "./kwilui.jpeg"
+import ShareplateMockup from "./shareplate.jpeg";
+import BolMockup from "./bol.png";
+import KwilUi from "./kwilui.jpeg";
+import ShareplateUI from "./shareplateui.jpeg";
 
 export const workCardsContent = [
-  { title: "Shareplate", description: "Social media for foodies", backgroundImage: `url(${ShareplateMockup})` },
-  { title: "bol", description: "Bowling progress analytics app" },
-  { title: "Kwil Block Explorer", description: "Blockchain explorer UI", backgroundImage: `url(${KwilUi})` },
-  { title: "Shareplate UI", description: "Food social media UI" },
+  {
+    title: "Shareplate",
+    description: "Social media for foodies",
+    backgroundImage: `url(${ShareplateMockup})`,
+  },
+  {
+    title: "bol",
+    description: "Bowling progress analytics app",
+    backgroundImage: `url(${BolMockup})`,
+  },
+  {
+    title: "Block Explorer UI",
+    description: "Blockchain explorer UI",
+    backgroundImage: `url(${KwilUi})`,
+  },
+  {
+    title: "Shareplate UI",
+    description: "Food social media UI",
+    backgroundImage: `url(${ShareplateUI})`,
+  },
   {
     title: "Set up PostgreSQL",
     description: "Set up PostgreSQL via Docker article",
